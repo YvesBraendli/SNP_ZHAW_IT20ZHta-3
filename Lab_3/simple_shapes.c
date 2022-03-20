3517adf0de5d5b1737374225f06ced8ee6c1f107
braenyve <braenyve@students.zhaw.ch>
2022-03-20 14:36:02 +0100

    created and finished task3 of lab3

--- Lab_3/simple_shapes.c
+++ Lab_3/simple_shapes.c
@@ -8,6 +8,14 @@
 #define WHT   "\x1B[37m"
 #define RESET "\x1B[0m"
 
+enum shape {OVAL, RECTANGLE};
+
+typedef struct {
+    int shape;
+    int size;
+    char *color;
+} Graphic;
+
 /* 
 Students: Create a new type "Graphic" that can store:
 - shape (OVAL, RECTANGLE) -> create a new enum for this type
@@ -45,15 +53,38 @@ int main() {
 
     do {
         printf("Geben Sie die gewünschte Form an [OVAL=0 | RECTANGLE=1]:");
-        scanf("%d", &input);
+        if (scanf("%d", &input)!=1||
+            (input!=1&&input!=0)){
+            printf("Geben sie entweder eine 0 oder eine 1 ein.\n");
+            exit(0);
+        }
+        graphic.shape = input;
         // Students: store the input in graphic
 
         printf("Geben Sie die gewünschte Grösse an:");
-        scanf("%u", &input);
+        if (scanf("%u", &input)!=1||input<1){
+            printf("Geben sie eine positive Zahl ein.\n");
+            exit(0);
+        }
+        graphic.size = input;
         // Students: store the input in graphic
 
         printf("Geben Sie die gewünschte Farb an [RED=0 | GREEN=1 | YELLOW=2]:");
-        scanf("%d", &input);
+        if (scanf("%d", &input)!=1||
+            (input != 1 && input != 0 && input != 2)){
+            printf("Geben sie entweder eine 0, eine 1 oder eine 2 ein.\n");
+            exit(0);
+        }
+        if (input == 0) {
+           graphic.color = RED;
+        }
+        if (input == 1) {
+           graphic.color = GRN;
+        }
+        if (input == 2) {
+           graphic.color = YEL;
+        }
+        
         // Students: store the input in graphic
         
         paint(graphic);
