ecebbcfe967ff1a4219eba6ba88afbb87856eb02
braenyve <braenyve@students.zhaw.ch>
2022-04-08 10:39:05 +0200

    added header files with basic structure

---
+++ Lab_6/work/personen-verwaltung/src/list.h
@@ -0,0 +1,15 @@
+#ifndef _MODEL_H_
+#define _MODEL_H_
+
+#define NAME_LEN 20
+
+typedef struct {
+  char         name[NAME_LEN];
+  char         first_name[NAME_LEN];
+  unsigned int age;
+} person_t;
+
+
+// Needs methods declaration for insert, remove and clear
+
+#endif // _MODEL_H_
